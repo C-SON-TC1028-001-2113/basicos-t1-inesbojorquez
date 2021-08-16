f7ecea53ef23d8691c2ae8d8fc188f014cad1f7a
inesbojorquez <a01252378@itesm.mx>
2021-08-16 21:47:16 +0000

    03 Promedio

--- assignments/03Promedio/src/exercise.py
+++ assignments/03Promedio/src/exercise.py
@@ -1,5 +1,10 @@
 def main():
     #escribe tu código abajo de esta línea
-
+    a = float(input('Calificacion de la materia1: '))
+    b = float(input('Calificacion de la materia2: '))
+    c = float(input('Calificacion de la materia3: '))
+    d = float(input('Calificacion de la materia4: '))
+    p= (a+b+c+d)/4
+    print('El promedio es: '+str(p))
 if __name__ == '__main__':
     main()
